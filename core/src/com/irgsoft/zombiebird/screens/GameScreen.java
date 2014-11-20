7ddf5b843f895c7a198b81f131684d8b0bc6cde0
Iván Rodríguez <mail@irodriguez.es>
2014-11-21 00:12:33 +0100

    Added basic rectangle moving in the screen

--- core/src/com/irgsoft/zombiebird/screens/GameScreen.java
+++ core/src/com/irgsoft/zombiebird/screens/GameScreen.java
@@ -2,7 +2,6 @@ package com.irgsoft.zombiebird.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
 import com.irgsoft.zombiebird.gameworld.GameRenderer;
 import com.irgsoft.zombiebird.gameworld.GameWorld;
 
@@ -16,18 +15,11 @@ public class GameScreen implements Screen {
 		Gdx.app.log(TAG, "attached");
 		
 		gameWorld = new GameWorld();
-		gameRenderer = new GameRenderer(gameWorld));
+		gameRenderer = new GameRenderer(gameWorld);
 	}
 
 	@Override
 	public void render(float delta) {
-		// Sets a color to fill the screen width (RGB = 10, 15, 230), Opacity of 1 (100%)
-//		Gdx.gl.glClearColor(10/255.0f, 15/255.0f, 230/255.0f, 1f);
-		// Fills the screen with the selected color
-//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		// Convert frame rate to String, print it
-//		Gdx.app.log("GameScreen FPS", (1/delta) + "");
-		
 		gameWorld.update(delta);
 		gameRenderer.render();
 	}
