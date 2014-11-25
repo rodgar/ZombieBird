47afb5650ecf7f6ab74130b61bb56718bf59363e
Iván Rodríguez <mail@irodriguez.es>
2014-11-25 23:19:25 +0100

    Finished gameplay and added sounds

--- core/src/com/irgsoft/zombiebird/gameworld/GameWorld.java
+++ core/src/com/irgsoft/zombiebird/gameworld/GameWorld.java
@@ -1,5 +1,7 @@
 package com.irgsoft.zombiebird.gameworld;
 
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
 import com.irgsoft.zombiebird.gameobjects.Bird;
 import com.irgsoft.zombiebird.gameobjects.ScrollHandler;
 import com.irgsoft.zombiebird.helpers.AssetLoader;
@@ -8,23 +10,38 @@ public class GameWorld {
 
 	private Bird bird;
 	private ScrollHandler scrollHandler;
-	private boolean isAlive = true;
+	private Rectangle ground;
 	
 	public GameWorld(int midPointY) {
 		bird = new Bird(33, midPointY - 5, 17, 12);
 		// The grass should start 66 points below the midPointY
 		scrollHandler = new ScrollHandler(midPointY + 66);
+		
+		ground = new Rectangle(0, midPointY + 66, 136, 11);
 	}
 	
 	public void update(float delta) {
+		// Add a delta cap so that if our game takes too long
+        // to update, we will not break our collision detection.
+		
+		if (delta > .15f) {
+			delta = .15f;
+		}
+		
 		bird.update(delta);
 		scrollHandler.update(delta);
 		
-		if (isAlive && scrollHandler.collides(bird)) {
+		if (scrollHandler.collides(bird) && bird.isAlive()) {
 			// Clean up on game over
 			scrollHandler.stop();
+			bird.die();
 			AssetLoader.dead.play();
-			isAlive = false;
+		}
+		
+		if (Intersector.overlaps(bird.getBoundingCircle(), ground)) {
+			scrollHandler.stop();
+			bird.die();
+			bird.decelerate();
 		}
 	}
 	
